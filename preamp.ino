914dc7e5d8b38ff5eba3659ad5eb1329d8ff9405
alexander ershov <alexander.ershov@gmailcom>
2015-11-08 18:23:42 +0300

    init

---
+++ preamp.ino
@@ -0,0 +1,33 @@
+
+#include <IRremote.h>
+
+const int RECV_PIN = P2_2;
+// REP FFFFFFFF
+// VUP FFA857
+// VDN FFE01F
+// CHDN FFA25D
+// CH FF629D
+// CHUP FFE21D
+// PAU 286101BD
+
+
+
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(5000);
+  Serial.println("IR TEST++++");
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+}
